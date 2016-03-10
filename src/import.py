eb11fc73cab131ebda9dffbd48f5f6e2ac8208e6
jaklxxm@gmail.com <jaklxxm@gmail.com>
2016-03-10 01:13:23 +0100

    nic

---
+++ src/import.py
@@ -0,0 +1,44 @@
+import sys
+import urllib2
+import xmltodict
+
+def main():
+    xml_file = "http://opendata.iprpraha.cz/feed.xml"	
+		
+    data = parse_xml(xml_file)	
+    for item in data['feed']['entry']: 
+        print '\n',item['title']
+	if 'Ortofoto' in item['title']: print ('too much XMLs !!')
+	if 'Ortofoto' not in item['title']: 
+	    xml_item = downXML(item)
+            subdata = parse_xml(xml_item)
+	    print xml_item  # nebude
+
+	    for subitem in subdata['feed']['entry']:
+	    	print '          ',subitem['category']['@label']
+	    	for links in subitem['link']:
+		    print '                    ',links['@href']
+    return 0
+
+
+
+def downXML(data):
+    for item in data['link']:
+	if item['@title'] in ('download','Download'): 
+	    xml_file = item['@href']
+    return (xml_file)
+
+
+def parse_xml(xml_file):
+    try:
+        fd = urllib2.urlopen(xml_file)
+    except urllib2.URLError as e:
+        sys.exit(e)
+        
+    obj = xmltodict.parse(fd.read())
+    fd.close()
+    return obj
+
+main()
+
+
