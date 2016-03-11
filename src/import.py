be894b1249a9c1cbd64f11a364c2ef20c335884d
Martin Jákl <jaklxxm@gmail.com>
2016-03-11 03:24:03 +0100

    new version same output

--- src/import.py
+++ src/import.py
@@ -3,43 +3,67 @@ import urllib2
 import xmltodict
 
 def main():
-    xml_file = "http://opendata.iprpraha.cz/feed.xml"	
-		
-    data = parse_xml(xml_file)	
-    for item in data['feed']['entry']: 
-        print '\n',item['title']
-	if 'Ortofoto' in item['title']: print ('too much XMLs !!')
+	xml_file = "http://opendata.iprpraha.cz/feed.xml"
+	data = parse_xml(xml_file)
+	alike = ''
+	if alike:
+		for item in data ['feed']['entry']:
+			exist=0 
+			if (alike in item['title']):
+				exist=1
+				item_print(item)
+	else:
+		for item in data['feed']['entry']: 
+			item_print(item)
+	return 0
+
+
+def item_print(item):
+	print '\n',item['title']
+	if 'Ortofoto' in item['title']: 
+		print (' -- too much links to display !! --')
 	if 'Ortofoto' not in item['title']: 
 		xml_item = downXML(item)
 		subdata = parse_xml(xml_item)
-		if isinstance(subdata['feed']['entry'],list):
-			for subitem in subdata['feed']['entry']:
-				print '     ',subitem['category']['@label']
-				for links in subitem['link']:
-					print '          ',links['@href']
-		else:
-			print '     ',subdata['feed']['entry']['category']['@label']
-			for links in subdata['feed']['entry']['link']:
-				print '          ',links['@href']
+		#print '    ',xml_item  # nebude
+		subitems_Links_print(subdata)
 
-    return 0
+
+def subitems_Links_print(subdata):
+	if isinstance(subdata['feed']['entry'],list):
+		for item in subdata['feed']['entry']: 
+			print '     ',item['category']['@label']
+			for links in item['link']:
+				print '          ',links['@href']#,'	',links['@title']
+	else:
+		print '          ',subdata['feed']['entry']['category']['@label']
+		for links in subdata['feed']['entry']['link']:
+			print '          ',links['@href']#,'	',links['@title']
+	return 0 
 
 
 
 def downXML(data):
-    for item in data['link']:
-	if item['@title'] in ('download','Download'): 
-	    xml_file = item['@href']
-    return (xml_file)
+	for item in data['link']:
+		if item['@title'] in ('download','Download'):
+			xml_file = item['@href']
+			#print xml_file
+
+	return xml_file
+
 
 
 def parse_xml(xml_file):
-    try:
-        fd = urllib2.urlopen(xml_file)
-    except urllib2.URLError as e:
-        sys.exit(e)
+	try:
+		fd = urllib2.urlopen(xml_file)
+	except urllib2.URLError as e:
+		sys.exit(e)
         
-    obj = xmltodict.parse(fd.read())
-    fd.close()
-    return obj
+	obj = xmltodict.parse(fd.read())
+	fd.close()
+	return obj
+
+
 main()
+
+
