5c9413c017b5bba3f4ba2263cabe15ad74a8840b
Martin Landa <landa.martin@gmail.com>
2016-04-11 20:09:37 +0200

    script for PG updates

---
+++ src/pg/upgrade_pgis_osm_bp.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+DB=pgis_osm_bp
+FILE=czech-republic-latest.osm.bz2
+
+# download
+wget http://download.geofabrik.de/openstreetmap/europe/$FILE
+
+# clean-up DB 
+psql $DB -c "DROP SCHEMA IF EXISTS osm CASCADE"
+psql $DB -c "CREATE SCHEMA osm"
+psql $DB -c "GRANT USAGE ON SCHEMA osm TO public"
+ 
+# import
+osm2pgsql -d $DB -E 3857 -p czech -s $FILE
+
+# czech_polygon: polygon -> multipolygon
+echo "ALTER TABLE czech_polygon ADD COLUMN way1 geometry(MultiPolygon,3857);
+UPDATE czech_polygon SET way1 = ST_Multi(way);
+ALTER TABLE czech_polygon DROP COLUMN way;
+ALTER TABLE czech_polygon RENAME COLUMN way1 TO way" | psql $DB
+
+# add primary key 'gid'
+# move tables to 'osm' schema
+# rename column way -> geom
+tables=`psql $DB -t -c "SELECT f_table_name FROM geometry_columns WHERE f_table_name LIKE 'czech%' and f_geometry_column = 'way'"`
+for table in $tables; do
+    echo "ALTER TABLE $table RENAME COLUMN way to geom;
+ALTER TABLE $table ADD COLUMN gid serial;
+ALTER TABLE $table ADD primary key (gid);
+ALTER TABLE $table SET SCHEMA osm;
+GRANT SELECT ON osm.$table TO public" | psql $DB
+done
+
+rm -rf $FILE
+
+exit 0
